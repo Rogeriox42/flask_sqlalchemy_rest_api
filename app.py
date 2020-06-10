d40110cffe772d7f93ac847e2c7c960664183b9f
Rogerio Rodrigues <sp.rogerio@outlook.com>
2020-06-10 02:05:31 -0300

    sqlite database / / �GET Product Route / POST Product route

--- app.py
+++ app.py
@@ -11,9 +11,63 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg':'Hello Flask'})
+#Init db 
+db = SQLAlchemy(app) 
+
+#Init marshmallow 
+ma = Marshmallow(app) 
+
+#Product Class/Model 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name 
+        self.description = description 
+        self.price = price 
+        self.quantity = quantity
+
+# Product Schema 
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'quantity')
+
+#Init Schema 
+# product_schema = ProductSchema(strict=True)
+# products_schema = ProductSchema(strict=True, many=True)
+
+product_schema = ProductSchema()
+products_schema = ProductSchema( many=True)
+
+#Create a Product 
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price'] 
+    quantity = request.json['quantity']
+
+    new_product = Product(name, description, price, quantity)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+#Get All Products 
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+
+
+
 
 #Run server 
 if __name__== '__main__': 
